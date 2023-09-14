5569f8fd848efb685ff1751fe135404afe5cf6a1
Khôi Tạ <khoita@Khois-MacBook-Pro.local>
2023-09-14 08:45:33 +0700

    enable cors

--- app/main.py
+++ app/main.py
@@ -1,16 +1,29 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
-from plato import predict
+from app.plato import predict
 import pickle
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class CustomUnpickler(pickle.Unpickler):
     def find_class(self, module, name):
         if name == "Plato":
-            from plato import Plato
+            from app.plato import Plato
 
             return Plato
         return super().find_class(module, name)
