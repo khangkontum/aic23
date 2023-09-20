a75aea4fd05e7680a10bdbdabf260a9a322bb9a6
khangkontum <khangkontum@gmail.com>
2023-09-20 14:00:18 +0700

    feat: add worker for extract_script.py

--- data_processing/extract_transcript.py
+++ data_processing/extract_transcript.py
@@ -1,8 +1,10 @@
-import whisper
+from multiprocessing import Pool
+from subprocess import call
 from tqdm import tqdm
-import torch
-import os
 import json
+import os
+import torch
+import whisper
 
 input_folder = "./raw/sounds"
 output_folder = "./raw/transcript"
@@ -12,9 +14,19 @@ if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model = whisper.load_model("large", device)
+model = whisper.load_model("base", device)
+
+def inference_and_save(paths):
+    input_path, output_path = paths
+    result = model.transcribe(input_path)
+
+    with open(output_path, "w+") as f:
+        json.dump(result, f, indent = 6, ensure_ascii=False)
+
 
 if __name__ == "__main__":
+
+    paths = []
     for root, _, files in os.walk(input_folder):
         for filename in tqdm(files):
             input_path = os.path.join(root, filename)
@@ -23,7 +35,11 @@ if __name__ == "__main__":
                 filename,
             ).replace(".mp3", ".json")
 
-            result = model.transcribe(input_path)
+            paths.append((input_path, output_path))
 
-            with open(output_path, "w+") as f:
-                json.dump(result, f, indent = 6, ensure_ascii=False)
+
+
+    pool = Pool(processes=4)
+    pool.map(inference_and_save, paths , 1) # Ensure the chunk size is 1
+    pool.close()
+    pool.join()
